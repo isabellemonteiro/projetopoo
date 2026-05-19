c75c33f42851bda5c6f56c455dd2b4309c8cd6a8
isabellemonteiro <isabelle.monteiro@escolar.ifrn.edu.br>
2026-05-19 09:27:34 -0300

    Add player and ghost classes with attributes

--- player.py
+++ player.py
@@ -1,1 +1,26 @@
+import pygame
+import time
 
+Class FormaJogador:
+  def __init__ (self):
+    self.nome = "base"
+    self.forca_pulo = -10
+    self.gravidade = 0.5
+    self.velocidade_movimento = 5 
+    self.pode_atravessar_obstaculos = false
+    self.causa_dano_impacto = false
+  def aplicar_fisica(self,jogador):
+    pass
+
+class FormaFantasma(FormaJogador):
+    def __init__(self):
+        super().__init__()
+        self.nome = "Fantasma"
+        self.forca_pulo = -15        
+        self.gravidade = 0.2         
+        self.velocidade_movimento = 6
+        self.pode_atravessar_obstaculos = True
+        self.causa_dano_impacto = False
+
+
+    
